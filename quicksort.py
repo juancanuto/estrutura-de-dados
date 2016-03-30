53027d39cd8ed023368a72b36b33791d465effb4
Juan Canuto Hassam <juan.hassam@fatec.sp.gov.br>
2016-03-30 09:57:50 -0300

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,10 +1,14 @@
 import unittest
+import random
+
+''' Pior caso O(n²) , Médio O(nlog n)'''
 
 def quicksort(seq):
     if len(seq) <= 1:
         return seq
     else:
-        pivo = seq[0]
+        pivo = seq[random.choice(range(0, len(seq)))]
+        print (pivo)
         iguais  = [x for x in seq if x == pivo]
         menores = [x for x in seq if x <  pivo]
         maiores = [x for x in seq if x >  pivo]
@@ -23,6 +27,10 @@ class OrdenacaoTestes(unittest.TestCase):
 
     def teste_lista_desordenada(self):
         self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], quicksort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 if __name__ == '__main__':
